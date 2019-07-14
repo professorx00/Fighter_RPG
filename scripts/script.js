ad9d0c6e48085378e806dccabffe0e0da7d4e5bf
Dave Ward <professorx00@gmail.com>
2019-07-14 19:59:39 -0400

    update the script

--- scripts/script.js
+++ scripts/script.js
@@ -139,84 +139,95 @@ $(document).ready(function () {
     let main = {
         health : 0,
         attack : 0,
-        // Initalize
-        //remove all elements in the divs
-        //Player Choice:
-        // title: Please select your character
-        //create button for select
-        //place characters in the arena
-        //On Click highlight character Choosen and assign data-player=true and src the player img remove from possible choice
         intialize: function () {
             docArena.empty();
             docArena.addClass("ArenaCharSelect");
-            $.each(characters, function (i, item) {
-                let holder = $("<div>").addClass("cSelectHolder");
-                let choice = $("<img>").attr("src", characters[i].smImg).addClass(`${characters[i].charName} charSelect`).attr("data-char", i);;
-                let cname = $("<span>").text(characters[i].charName);
-                holder.append(choice, cname);
-                docArena.append(holder);
-                holder.on("click", function (event) {
-                    if (!playerChoice) {
-                        choice.addClass("Selected");
-                        holder.addClass("boxSelected");
-                        playerChoice = true;
-                    }
-                    else {
-                        $(".boxSelected").css("border", "").removeClass("boxSelected");
-                        $(".Selected").removeClass("Selected");
-                        choice.addClass("Selected");
-                        holder.addClass("boxSelected");
-                    }
-                });
-            });
+            playerChoice = getCharacters(characters, playerChoice);
             let instruction = $("<span>");
             instruction.text("Please Select Your Character");
             docArena.append(instruction);
             let count=0;
-            $("#select").on("click", function (event) {
-                if(count==0){
-                    playerChar = characters[$(".Selected").attr("data-char")];
-                    console.log(playerChar.charName);
-                    $(".boxSelected").css("display", "none");
-                    docPlayIcon.attr("src",playerChar.smImg);
-                    docPlayer.text(playerChar.charName);
-                    docPH.text(playerChar.playerHealth);
-                    docPA.text(playerChar.playerAttack);
-                    this.health = parseInt(playerChar.playerHealth);
-                    this.attack = parseInt(playerChar.playerAttack);
-                    $(".playerList").css("display","inline");
-                    count+=1;
-                }
-                else if(count>0 && count<3){
-                    enemyChoice.push(characters[$(".Selected").attr("data-char")]);
-                    let enemy = $(".Selected");
-                    let enemyBox = $(".boxSelected");
-                    let temp = $(enemyBox);
-                    temp.addClass("boxEnemy")
-                    docEnemyImg.append(temp);
-                    count+=1;
-                }
-                else if(count>2 && count<4){
-                    enemyChoice.push(characters[$(".Selected").attr("data-char")]);
-                    let enemy = $(".Selected");
-                    let enemyBox = $(".boxSelected");
-                    let temp = $(enemyBox);
-                    temp.addClass("boxEnemy")
-                    docEnemyImg.append(temp);
-                    count+=1;
-                    docArena.empty();
-                }
-                else{
-                    docArena.empty();
-                    count+=1;
-                }
-                
-
-            });
+            ({ count, playerChar } = selection(count, playerChar, characters, enemyChoice));
+            instruction.text("Please Choose the enemy you wish to face first");
+            docArena.append(instruction);
+            this.play();
         },//Intialization of game End
+        play: function(){
+            console.log("play" + $(".Selected"));
+        }
     };//JSON OBJECT Main End
 
 
     main.intialize();
 
 });//document.ready end
+
+function getCharacters(characters, playerChoice) {
+    $.each(characters, function (i, item) {
+        let holder = $("<div>").addClass("cSelectHolder");
+        let choice = $("<img>").attr("src", characters[i].smImg).addClass(`${characters[i].charName} charSelect`).attr("data-char", i);
+        ;
+        let cname = $("<span>").text(characters[i].charName);
+        holder.append(choice, cname);
+        docArena.append(holder);
+        holder.on("click", function (event) {
+            
+            if (!playerChoice) {
+                choice.addClass("Selected");
+                holder.addClass("boxSelected");
+                playerChoice = true;
+            }
+            else {
+                $(".boxSelected").css("border", "").removeClass("boxSelected");
+                $(".Selected").removeClass("Selected");
+                choice.addClass("Selected");
+                holder.addClass("boxSelected");
+            }
+        });
+    });
+    return playerChoice;
+}
+
+function selection(count, playerChar, characters, enemyChoice) {
+    $("#select").on("click", function (event) {
+        if (count == 0) {
+            playerChar = characters[$(".Selected").attr("data-char")];
+            $(".boxSelected").css("display", "none");
+            docPlayIcon.attr("src", playerChar.smImg);
+            docPlayer.text(playerChar.charName);
+            docPH.text(playerChar.playerHealth);
+            docPA.text(playerChar.playerAttack);
+            this.health = parseInt(playerChar.playerHealth);
+            this.attack = parseInt(playerChar.playerAttack);
+            $(".playerList").css("display", "inline");
+            count += 1;
+        }
+        else if (count > 0 && count < 3) {
+            enemyChoice.push(characters[$(".Selected").attr("data-char")]);
+            let enemy = $(".Selected");
+            let enemyBox = $(".boxSelected");
+            let temp = $(enemyBox);
+            temp.addClass("boxEnemy");
+            docEnemyImg.append(temp);
+            count += 1;
+        }
+        else if (count > 2 && count < 4) {
+            enemyChoice.push(characters[$(".Selected").attr("data-char")]);
+            let enemy = $(".Selected");
+            let enemyBox = $(".boxSelected");
+            let temp = $(enemyBox);
+            temp.addClass("boxEnemy");
+            docEnemyImg.append(temp);
+            count += 1;
+            docArena.empty();
+            
+        }
+        else {
+            docArena.empty();
+            count += 1;
+        }
+        
+    });
+    return { count, playerChar };
+}
+
